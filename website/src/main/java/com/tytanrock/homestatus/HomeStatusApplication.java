310586b98fda5fee602ea85dc00c095f04f9ce89
TytanRock <coryness@tytan.rocks>
2021-08-22 17:31:51 -0400

    Moved board docs to board subdirectory & added website code

---
+++ website/src/main/java/com/tytanrock/homestatus/HomeStatusApplication.java
@@ -0,0 +1,13 @@
+package com.tytanrock.homestatus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeStatusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeStatusApplication.class, args);
+	}
+
+}
---
+++ website/src/test/java/com/tytanrock/homestatus/HomeStatusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tytanrock.homestatus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HomeStatusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
